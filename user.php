fcaac71bc673fc93cf4df0e22deaf4fa07f4dceb
Satish <mukkojusatish@gmail.com>
2015-12-30 19:59:45 +0530

    password changed

--- user.php
+++ user.php
@@ -3,7 +3,7 @@
 class User {
   
   public function getRecent(){
-    $db =  new PDO('mysql:host=localhost;dbname=saddahaq_facebook_apps', 'root', 'dambo');
+    $db =  new PDO('mysql:host=localhost;dbname=saddahaq_facebook_apps', 'root', 'vivenfarms');
     $tmp = $db->query("SELECT _ID_ id FROM table_free_basics_saddahaq ORDER BY _Tme_ DESC LIMIT 0,40");
     $res = $tmp->fetchAll(PDO::FETCH_ASSOC);
     return $res[0];
