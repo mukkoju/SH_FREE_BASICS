522e560d08f305dfdcd1a4469a01d92208c22a96
Satish <mukkojusatish@gmail.com>
2015-12-31 12:42:40 +0530

    list length increased

--- user.php
+++ user.php
@@ -4,7 +4,7 @@ class User {
   
   public function getRecent(){
     $db =  new PDO('mysql:host=localhost;dbname=saddahaq_facebook_apps', 'root', 'vivenfarms');
-    $tmp = $db->query("SELECT _ID_ id FROM table_free_basics_saddahaq ORDER BY _Tme_ DESC LIMIT 0,40");
+    $tmp = $db->query("SELECT _ID_ id FROM table_free_basics_saddahaq ORDER BY _Tme_ DESC LIMIT 0,42");
     $res = $tmp->fetchAll(PDO::FETCH_ASSOC);
     return $res[0];
   }
